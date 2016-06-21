2054eabc8829a069fee25d497dec20b2361f855a
hank <459673409@qq.com>
2016-06-21 23:36:48 +0800

    首页登录逻辑adminLogin.js添加ajax请求

--- WebRoot/adminLogin.js
+++ WebRoot/adminLogin.js
@@ -1,7 +1,20 @@
 function loginCheck() {
 	if ($('#username').val() == "" || $('#password').val() == "") {
 		alert("Can not null");
+	} else {
+		$.ajax({
+			data : { // The data sent to the database
+				userType : $("#userType").val(),
+				username : $("#username").val(),
+				password : $("#password").val()
+			},
+			dataType : "json",
+			success : function() {
+
+			},
+			type : "post",
+			url : login_validate
+		});
 	}
-	;
 
 }
